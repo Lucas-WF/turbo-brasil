0f2515f8470f3f059309b555104aae1a93cb21eb
Miller <mmillermonteiro@gmail.com>
2024-09-23 12:11:44 -0300

    Backgrounds and roads added

--- src/main/road/Road.java
+++ src/main/road/Road.java
@@ -3,7 +3,12 @@ package main.road;
 import main.engine.GamePanel;
 import main.entity.Player;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,7 +18,7 @@ public class Road {
     private final int segmentLength;
     private int numSegments;
     private final double camD; //CamDepth
-    private String mapSeleted;
+    private BufferedImage backgroundImage;
 
     Color grass, rumble, road, midline;
 
@@ -24,7 +29,7 @@ public class Road {
         this.camD = camD;
     }
 
-    private void generateRoad() {
+    private void generateRoad() throws IOException {
         String mapSelected = GamePanel.getMapSelected();
         if (mapSelected == null) {
             throw new IllegalStateException("The selected map cannot be null");
@@ -32,6 +37,7 @@ public class Road {
         switch (mapSelected) {
             case "Rio":
                 this.numSegments = 2000;
+                this.backgroundImage = ImageIO.read(new File("res/background/rioBackground.png"));
                 for (int i = 0; i < numSegments; i++) {
                     Line line = new Line();
                     line.setZ(i * segmentLength);
@@ -45,10 +51,34 @@ public class Road {
                 }
                 break;
             case "Caatinga":
-                // Add "Caatinga" map logic here
+                this.numSegments = 2200;
+                this.backgroundImage = ImageIO.read(new File("res/background/caatingaBackground.png"));
+                for (int i = 0; i < numSegments; i++) {
+                    Line line = new Line();
+                    line.setZ(i * segmentLength);
+                    if (i > 100 && i < 200 || i > 400 && i < 500 || i > 1000  && i < 1375 || i > 1825 && i < 1925) {
+                        line.setCurve(4);
+                    }
+                    if (i > 200 && i < 300 || i > 500 && i < 600) {
+                        line.setCurve(-4);
+                    }
+                    lines.add(line);
+                }
                 break;
             case "Amazonia":
-                // Add "Amazonia" map logic here
+                this.numSegments = 2000;
+                this.backgroundImage = ImageIO.read(new File("res/background/amazoniaBackground.png"));
+                for (int i = 0; i < numSegments; i++) {
+                    Line line = new Line();
+                    line.setZ(i * segmentLength);
+                    if (i > 200 && i < 300 || i > 400 && i < 500 ||i > 1400 && i < 1600 || i > 1825 && i < 1950) {
+                        line.setCurve(4);
+                    }
+                    if (i > 300 && i < 400 || i > 900 && i < 1200 ) {
+                        line.setCurve(-4);
+                    }
+                    lines.add(line);
+                }
                 break;
             default:
                 throw new IllegalArgumentException("Unknown map selected: " + mapSelected);
@@ -60,7 +90,13 @@ public class Road {
     }
 
     public void renderRoad(Graphics2D g, int width, int height, int camX, int camY, int pos, int playerX, Player player) {
-        generateRoad();
+        try {
+            generateRoad();
+            g.drawImage(backgroundImage, 0, 0, width, height / 2 + 10, null);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         int startPos = pos / segmentLength;
         double x = 0, dx = 0;
         double maxY = height;
@@ -75,7 +111,7 @@ public class Road {
 
             if (currentLine.getY() > 0 && currentLine.getY() < maxY) {
                 maxY = currentLine.getY();
-                switch(GamePanel.getMapSelected()) {
+                switch (GamePanel.getMapSelected()) {
                     case "Rio":
                         grass = ((n / 2) % 2) == 0 ? new Color(60, 120, 220) : new Color(90, 139, 227);
                         rumble = ((n / 2) % 2) == 0 ? new Color(120, 120, 120) : new Color(70, 70, 70);
@@ -107,11 +143,33 @@ public class Road {
                 drawQuad(g, midline, (int) previousLine.getX(), (int) previousLine.getY(), (int) (previousLine.getW() * 0.8), (int) currentLine.getX(), (int) currentLine.getY(), (int) (currentLine.getW() * 0.8));
                 drawQuad(g, road, (int) previousLine.getX(), (int) previousLine.getY(), (int) (previousLine.getW() * 0.7), (int) currentLine.getX(), (int) currentLine.getY(), (int) (currentLine.getW() * 0.7));
             }
-        }
-        if (700 >= pos / segmentLength && pos / segmentLength >= 200){
-            player.setCurve(player.getSpeed() * 1/10000);
-        } else {
-            player.setCurve(0);
+
+            String mapSelected = GamePanel.getMapSelected();
+            switch (mapSelected) {
+                case "Rio":
+                    if (startPos > 75 && startPos < 375 || startPos > 825 && startPos < 925 || startPos > 1075 && startPos < 1375 || startPos > 1825 && startPos < 1925 || startPos > 525 && startPos < 625 || startPos > 1525 && startPos < 1625) {
+                        player.setCurve(player.getSpeed() * 1 / 10000);
+                    } else {
+                        player.setCurve(0);
+                    }
+                    break;
+                case "Caatinga":
+                    if (startPos > 100 && startPos < 200 || startPos > 400 && startPos < 500 || startPos > 1000  && startPos < 1375 || startPos > 1825 && startPos < 1925 || startPos > 200 && startPos < 300 || startPos > 500 && startPos < 600) {
+                        player.setCurve(player.getSpeed() * 1 / 10000);
+                    } else {
+                        player.setCurve(0);
+                    }
+                    break;
+                case "Amazonia":
+                    if (startPos > 200 && startPos < 300 || startPos > 400 && startPos < 500 || startPos > 1400 && startPos < 1600 || startPos > 1825 && startPos < 1950 || startPos > 300 && startPos < 400 || startPos > 900 && startPos < 1200 ) {
+                        player.setCurve(player.getSpeed() * 1 / 10000);
+                    } else {
+                        player.setCurve(0);
+                    }
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
